d9f3f735cb13daea74974627d200859053bd6dd5
SuperDotNSW <superdotnsw@gmail.com>
2026-05-15 17:52:38 +1000

    Established communication with TSH webserver

---
+++ requests_test.py
@@ -0,0 +1,95 @@
+import requests
+
+BASE_URL = "http://localhost:5000"
+
+class Ruleset():
+    banByMaxGames:dict = {}
+    banCount:int = 3
+    counterpickStages:list = []
+    neutralStages:list = []
+    errors = []
+    name:str = ""
+    strikeOrder:dict = { 
+        0: 1, 
+        1: 2, 
+        2: 1
+    }
+    useDSR:bool = False
+    useMDSR:bool = True
+    videogame:str = ""
+    
+    def update_from_tsh_data(self, webdata:dict):
+        d = webdata['ruleset']
+        self.banByMaxGames = d['banByMaxGames']
+        self.banCount = d['banCount']
+        self.counterpickStages = d['counterpickStages']
+        self.errors = d['errors']
+        self.name = d['name']
+        self.neutralStages = d['neutralStages']
+        self.strikeOrder = d['strikeOrder']
+        self.useDSR = d['useDSR']
+        self.useMDSR = d['useMDSR']
+        self.videogame = d['videogame']
+    
+    def __init__(self, webdata:dict):
+        self.update_from_tsh_data(webdata)
+
+class State():
+    # From the 'state' object retrieved from /ruleset
+    canRedo:bool = False
+    canUndo:bool = True
+    currGame:int = 0
+    currPlayer:int = 0
+    currStep:int = 0
+    gentlemans:bool = False
+    lastWinner:int = 0
+    selectedStage:dict = {}
+    stagesPicked:list = []
+    stagesWon:dict = {}
+    strikedBy:dict = {}
+    strikedStages:dict = {}
+
+    class Player():
+        display_name:str = ""
+        discord_user_id:int = 0
+
+        def __init__(self, display_name:str="", discord_user_id:int=0):
+            self.display_name = display_name
+            self.discord_user_id = discord_user_id
+    
+    p1:Player = Player()
+    p2:Player = Player()
+
+    def update_from_tsh_data(self, webdata:dict):
+        d = webdata['state']
+        self.canRedo = d['canRedo']
+        self.canUndo = d['canUndo']
+        self.currGame = d['currGame']
+        self.currPlayer = d['currPlayer']
+        self.currStep = d['currStep']
+        self.gentlemans = d['gentlemans']
+        self.lastWinner = d['lastWinner']
+        self.selectedStage = d['selectedStage']
+        self.stagesPicked = d['stagesPicked']
+        self.stagesWon = d['stagesWon']
+        self.strikedBy = d['strikedBy']
+        self.strikedStages = d['strikedStages']
+
+        p1.display_name = webdata['p1']
+        p2.display_name = webdata['p2']
+    
+    def __init__(self, webdata:dict):
+        self.update_from_tsh_data(webdata)
+
+# REQUEST EXAMPLE
+webdata = requests.get(f"{BASE_URL}/ruleset").json()
+ruleset = Ruleset(webdata)
+state = State(webdata)
+
+teststage = ruleset.neutralStages[0]
+
+print(ruleset.neutralStages)
+
+# POST EXAMPLE
+post = requests.post(f"{BASE_URL}/stage_strike_stage_clicked", json=teststage)
+print(post)
